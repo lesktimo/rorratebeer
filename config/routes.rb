37c9a2a8142f032ead57f0cc0b36855c8ee5eddf
lesktimo <l3skt1m@gmail.com>
2017-02-05 18:51:56 +0200

    initial login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
   root 'breweries#index'
@@ -6,7 +7,10 @@ Rails.application.routes.draw do
   #get 'ratings/new', to: 'ratings#new'
   #post 'ratings', to: 'ratings#create'
 
+  get 'signup', to: 'users#new'
+
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126093054) do
+ActiveRecord::Schema.define(version: 20170205160802) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +30,12 @@ ActiveRecord::Schema.define(version: 20170126093054) do
   create_table "ratings", force: :cascade do |t|
     t.integer  "score"
     t.integer  "beer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
